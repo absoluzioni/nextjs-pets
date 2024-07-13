426d77a55d2bcd562ccc7114d8507928b178ff5f
Andrea <absoluzioni@gmail.com>
2024-07-13 12:33:11 +0200

    added the state for unreadCount and useEffect for send hardcode message using Pusher

--- app/submit-contact-action/route.js
+++ app/submit-contact-action/route.js
@@ -1,42 +1,42 @@
-import { ObjectId } from "mongodb"
-import clientPromise from "../../lib/mongodb"
-import { NextResponse } from "next/server"
-const sanitizeHtml = require("sanitize-html")
-const nodemailer = require("nodemailer")
+import { ObjectId } from "mongodb";
+import clientPromise from "../../lib/mongodb";
+import { NextResponse } from "next/server";
+const sanitizeHtml = require("sanitize-html");
+const nodemailer = require("nodemailer");
 
 const sanitizeOptions = {
   allowedTags: [],
   allowedAttributes: {}
-}
+};
 
 export async function POST(request) {
-  const incoming = await request.json()
+  const incoming = await request.json();
 
-  console.log(incoming)
+  console.log(incoming);
   // borrow code from express version etc...
-  if (incoming.answer.toUpperCase() !== "PUPPY") {
-    return NextResponse.json({ message: "Sorry" })
+  if (incoming.answer.toUpperCase() !== process.env.CONTACTFORMSECRET) {
+    return NextResponse.json({ message: "Sorry" });
   }
 
   const ourObject = {
     name: sanitizeHtml(incoming.name, sanitizeOptions),
     email: sanitizeHtml(incoming.email, sanitizeOptions),
     comment: sanitizeHtml(incoming.comment, sanitizeOptions)
-  }
+  };
 
   if (!ObjectId.isValid(incoming.petId)) {
-    return NextResponse.json({ message: "Bad id" })
+    return NextResponse.json({ message: "Bad id" });
   }
 
-  ourObject.petId = new ObjectId(incoming.petId)
-  const client = await clientPromise
+  ourObject.petId = new ObjectId(incoming.petId);
+  const client = await clientPromise;
   const doesPetExist = await client
     .db()
     .collection("pets")
-    .findOne({ _id: new ObjectId(ourObject.petId) })
+    .findOne({ _id: new ObjectId(ourObject.petId) });
 
   if (doesPetExist) {
-    await client.db().collection("contacts").insertOne(ourObject)
+    await client.db().collection("contacts").insertOne(ourObject);
 
     // send mailtrap emails here
     var transport = nodemailer.createTransport({
@@ -46,14 +46,14 @@ export async function POST(request) {
         user: process.env.MAILTRAPUSERNAME,
         pass: process.env.MAILTRAPPASSWORD
       }
-    })
+    });
 
     transport.sendMail({
       to: ourObject.email,
       from: "admin@localhost",
       subject: `Thank you for your interest in ${doesPetExist.name}`,
       html: `<h3 style="color: #475aff; font-size: 30px; font-weight: normal;">Thank you!</h3><p>We appreciate your interest in ${doesPetExist.name} and one of our staff members will reach out to you shortly! Below is a copy of the message you sent us for your personal recrods:</p><p><em>${ourObject.comment}</em></p>`
-    })
+    });
 
     transport.sendMail({
       to: "adoptioncenter@localhost",
@@ -65,14 +65,14 @@ export async function POST(request) {
       Email: ${ourObject.email}<br>
       Message: ${ourObject.comment}
       </p>`
-    })
+    });
 
-    console.log("email sent")
+    console.log("email sent");
 
-    return NextResponse.json({ message: "Success" })
+    return NextResponse.json({ message: "Success" });
   }
 
-  res.json({ message: "No way" })
+  res.json({ message: "No way" });
 
   // check for puppy as the answer etc...
 
@@ -80,5 +80,5 @@ export async function POST(request) {
 
   // const client = await clientPromise
   // client.db().collection("pets").insertOne(ourObject)
-  return NextResponse.json({ message: "No way" })
+  return NextResponse.json({ message: "No way" });
 }
--- lib/chat.jsx
+++ lib/chat.jsx
@@ -1,15 +1,58 @@
-const { useState } = require("react");
+const { useState, useEffect } = require("react");
+import Pusher from "pusher-js";
 
 export default function Chat() {
-  const [isOpenChat, setIsOpenChat] = useState(false);
+  const [isChatOpen, setIsChatOpen] = useState(false);
+  const [unreadCount, setUnreadCount] = useState(0);
+  const [socketId, setSocketId] = useState();
+
+  useEffect(() => {
+    const pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHERKEY, {
+      cluster: "eu"
+    });
+
+    pusher.connection.bind("connected", () => {
+      setSocketId(pusher.connection.socket_id);
+    });
+
+    const privateChannel = pusher.subscribe("private-petchat");
+    privateChannel.bind("message", data => {
+      console.log(data);
+    });
+  }, []);
 
   function toggleChatClick() {
-    setIsOpenChat(!isOpenChat);
+    setIsChatOpen(!isChatOpen);
+    if (isChatOpen) {
+      setUnreadCount(0);
+    }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch("/admin/send-chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        message: "Unicorn pizza",
+        socket_id: socketId
+      })
+    });
   }
 
   return (
     <>
       <div className="open-chat" onClick={toggleChatClick}>
+        <span className={`open-chat__inbox ${!unreadCount ? "hidden" : ""}`}>
+          {unreadCount}
+        </span>
+        {/* la stessa cosa la farebbe questa riga di codice */}
+        {unreadCount > 0 && (
+          <span className="open-chat__inbox">{unreadCount}</span>
+        )}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="22"
@@ -21,7 +64,7 @@ export default function Chat() {
           <path d="M16 8c0 3.866-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7M4.5 5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h7a.5.5 0 0 0 0-1zm0 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1z" />
         </svg>
       </div>
-      <div className={`chat-box ${isOpenChat ? "chat-box--visible" : ""}`}>
+      <div className={`chat-box ${isChatOpen ? "chat-box--visible" : ""}`}>
         <div className="chat-box__header">
           <h4 className="chat-box__title">Staff Team Chat</h4>
           <svg
@@ -48,7 +91,7 @@ export default function Chat() {
             <p>Sure, no problem.</p>
           </div>
         </div>
-        <form className="chat-box__form">
+        <form className="chat-box__form" onSubmit={handleSubmit}>
           <input
             type="message"
             name="message"
